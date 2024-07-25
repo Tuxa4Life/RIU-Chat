29ae2dc1e94c1f19b786fd716a80c1c463f5fa11
Nikoloz Tukhashvili <66478283+Tuxa4Life@users.noreply.github.com>
2024-07-25 21:23:23 +0400

    API Data Hidden

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { collection, getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDyKxZQMc3tKgpofU4-DP1MmA5UFT8yyyY", // if you see this, please don't...
-  authDomain: "flounge-v2.firebaseapp.com",
-  projectId: "flounge-v2",
-  storageBucket: "flounge-v2.appspot.com",
-  messagingSenderId: "412477555964",
-  appId: "1:412477555964:web:a584538523bdd2e0904379"
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig)
 
